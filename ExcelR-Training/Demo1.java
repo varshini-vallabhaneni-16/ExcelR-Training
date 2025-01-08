2fb2968853e4f6122fb26c28aaa18e39756b81a1
varshini-vallabhaneni-16 <varshinivallabhaneni16@gmail.com>
2025-01-08 09:15:06 +0530

    Add files via upload
    String Loops

---
+++ ExcelR-Training/Demo1.java
@@ -0,0 +1,18 @@
+public class Demo1 {
+    public static void main(String[] args) {
+        String c="varshini";
+        System.out.println(c.length());
+        System.out.println(c.charAt(0));
+        //substring
+System.out.println(c.substring(2,3));
+System.out.println("hello".equals("Hello"));
+System.out.println(c.substring(2,3));
+System.out.println(c.substring(c.length()-1));
+System.out.println("a"+"b");
+System.out.println("hello" +"(2+2)");
+for( int i=0;i<11;i++){
+System.out.println("varshini");}
+
+    }
+    
+}
