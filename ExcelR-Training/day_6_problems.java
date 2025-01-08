d25b6041a73ede707ef4b7ed7b6690ba8cb0c729
varshini-vallabhaneni-16 <varshinivallabhaneni16@gmail.com>
2025-01-08 10:29:53 +0530

    Add files via upload

---
+++ ExcelR-Training/day_6_problems.java
@@ -0,0 +1,11 @@
+ class Day_6_problems{
+    public static void main(String[] args) {
+       //outerloop
+        for(int i=0;i<3;i++){
+            for(int j=0;j<4;j++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+}
+}
