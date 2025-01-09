e2418136cdd0cafab7d3e301724275ca2d0e4497
varshini-vallabhaneni-16 <varshinivallabhaneni16@gmail.com>
2025-01-09 13:31:41 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,18 @@
+
+public class Student {
+    String name;
+    int id;
+    String classNo;
+    String dept;
+    int phNumber;
+    public void talk(){
+        System.out.println("students are talking");
+    }
+    public void makeNoise(){
+        System.out.println("students are making noise");
+    }
+    public void coding(){
+        System.out.println("students are coding");
+    }
+}
+
---
+++ Test.java
@@ -0,0 +1,14 @@
+
+public class Test {
+  public static void main(String[] args) {
+    //  int[] arr=new int[5];
+    Student std1 = new Student();
+      
+      System.out.println(std1.name);
+       std1.name="varshini";
+       std1.id=88;
+     std1.classNo="g2";
+      std1.dept="cse";
+       std1.phNumber=12;
+  }
+  }  
